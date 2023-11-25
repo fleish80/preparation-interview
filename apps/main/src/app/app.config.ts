658012599b4238ca7bbe29f8b6f44c196bc097ad
fleish80 <Fleish.D@gmail.com>
2023-11-26 00:21:56 +0200

    preparation

--- apps/main/src/app/app.config.ts
+++ apps/main/src/app/app.config.ts
@@ -1,9 +1,8 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
